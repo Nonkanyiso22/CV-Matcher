8c8c9211b1104859fb8e9a50b6db67edf54422e9
Nonkanyiso22 <nonkanyisothembelihle70@gmail.com>
2026-03-19 14:52:54 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace CVMatcherApp
+{
+    partial class CVMatcherForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCV = new System.Windows.Forms.TextBox();
+            this.txtJob = new System.Windows.Forms.TextBox();
+            this.btnAnalyse = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblCVIput = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCV
+            // 
+            this.txtCV.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCV.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtCV.Location = new System.Drawing.Point(21, 67);
+            this.txtCV.Multiline = true;
+            this.txtCV.Name = "txtCV";
+            this.txtCV.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtCV.Size = new System.Drawing.Size(152, 28);
+            this.txtCV.TabIndex = 0;
+            this.txtCV.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtJob
+            // 
+            this.txtJob.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtJob.ForeColor = System.Drawing.Color.Black;
+            this.txtJob.Location = new System.Drawing.Point(240, 67);
+            this.txtJob.Multiline = true;
+            this.txtJob.Name = "txtJob";
+            this.txtJob.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtJob.Size = new System.Drawing.Size(157, 28);
+            this.txtJob.TabIndex = 1;
+            // 
+            // btnAnalyse
+            // 
+            this.btnAnalyse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnAnalyse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAnalyse.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnalyse.ForeColor = System.Drawing.Color.White;
+            this.btnAnalyse.Location = new System.Drawing.Point(158, 124);
+            this.btnAnalyse.Name = "btnAnalyse";
+            this.btnAnalyse.Size = new System.Drawing.Size(135, 63);
+            this.btnAnalyse.TabIndex = 2;
+            this.btnAnalyse.Text = "Analyse Match";
+            this.btnAnalyse.UseVisualStyleBackColor = false;
+            this.btnAnalyse.Click += new System.EventHandler(this.btnAnalyse_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblResult.Location = new System.Drawing.Point(-3, 223);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(812, 229);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "Result will appear here";
+            // 
+            // lblCVIput
+            // 
+            this.lblCVIput.AutoSize = true;
+            this.lblCVIput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCVIput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCVIput.ForeColor = System.Drawing.Color.Black;
+            this.lblCVIput.Location = new System.Drawing.Point(21, 38);
+            this.lblCVIput.Name = "lblCVIput";
+            this.lblCVIput.Size = new System.Drawing.Size(92, 26);
+            this.lblCVIput.TabIndex = 4;
+            this.lblCVIput.Text = "CV Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(240, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Job Description";
+            // 
+            // CVMatcherForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCVIput);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnAnalyse);
+            this.Controls.Add(this.txtJob);
+            this.Controls.Add(this.txtCV);
+            this.Name = "CVMatcherForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Skill Analyzer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCV;
+        private System.Windows.Forms.TextBox txtJob;
+        private System.Windows.Forms.Button btnAnalyse;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblCVIput;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CVMatcherApp
+{
+    public partial class CVMatcherForm : Form
+    {
+        public CVMatcherForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAnalyse_Click(object sender, EventArgs e)
+        {
+       
+            string cvText = txtCV.Text.ToLower();
+            string jobText = txtJob.Text.ToLower();
+
+            var cvWords = cvText.Split(' ');
+            var jobWords = jobText.Split(' ');
+
+            var matches = jobWords.Where(word => cvWords.Contains(word)).Distinct().ToList();
+            var missing = jobWords.Where(word => !cvWords.Contains(word)).Distinct().ToList();
+
+            int matchScore = (matches.Count * 100) / (jobWords.Length == 0 ? 1 : jobWords.Length);
+
+            lblResult.Text = $"Match Score: {matchScore}%\n\n" +
+                             $"Matched Skills:\n{string.Join(", ", matches.Take(10))}\n\n" +
+                             $"Missing Skills:\n{string.Join(", ", missing.Take(10))}";
+        }
+
+    }
+}
+
